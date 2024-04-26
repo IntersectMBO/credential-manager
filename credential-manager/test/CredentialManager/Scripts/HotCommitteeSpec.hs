63b45a6ce617bef049140c583a7349e8a19b65d3
Jamie Bertram <jamie.bertram@tweag.io>
2024-04-26 12:29:34 -0400

    Add hot and cold credential script test suites

---
+++ credential-manager/test/CredentialManager/Scripts/HotCommitteeSpec.hs
@@ -0,0 +1,99 @@
+module CredentialManager.Scripts.HotCommitteeSpec where
+
+import CredentialManager.Gen ()
+import CredentialManager.Scripts.HotCommittee (
+  ScriptContext (..),
+  ScriptPurpose (..),
+  TxInInfo (..),
+  TxInfo (..),
+  hotCommitteeScript,
+ )
+import PlutusLedgerApi.V3 (
+  Address,
+  BuiltinData,
+  CurrencySymbol,
+  OutputDatum,
+  ScriptHash,
+  TokenName,
+  TxOut (..),
+  Value (Value, getValue),
+ )
+import qualified PlutusTx.AssocMap as AMap
+import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
+
+spec :: Spec
+spec = do
+  prop "Invariant 1: Fails if not voting" invariant1BadPurpose
+  prop "Invariant 2: Fails if NFT not spent" invariant2NFTMissing
+  prop "Invariant 2: Succeeds if NFT spent" invariant3NFTSpent
+
+invariant1BadPurpose
+  :: CurrencySymbol -> BuiltinData -> ScriptContext -> Property
+invariant1BadPurpose policyId redeemer ctx = case scriptContextPurpose ctx of
+  Voting{} -> discard
+  _ -> hotCommitteeScript policyId redeemer ctx === False
+
+invariant2NFTMissing
+  :: CurrencySymbol -> BuiltinData -> ScriptContext -> Property
+invariant2NFTMissing policyId redeemer ctx =
+  hotCommitteeScript policyId redeemer ctx' === False
+  where
+    ctx' =
+      ctx
+        { scriptContextTxInfo = case ctx of
+            ScriptContext{..} ->
+              scriptContextTxInfo
+                { txInfoInputs = do
+                    TxInInfo{..} <- txInfoInputs scriptContextTxInfo
+                    let TxOut{..} = txInInfoResolved
+                    pure
+                      TxInInfo
+                        { txInInfoResolved =
+                            TxOut
+                              { txOutValue = Value $ AMap.delete policyId $ getValue txOutValue
+                              , ..
+                              }
+                        , ..
+                        }
+                }
+        }
+
+invariant3NFTSpent
+  :: CurrencySymbol
+  -> BuiltinData
+  -> TxInfo
+  -> BuiltinData
+  -> BuiltinData
+  -> Address
+  -> OutputDatum
+  -> Maybe ScriptHash
+  -> Value
+  -> AMap.Map TokenName Integer
+  -> TokenName
+  -> Integer
+  -> Property
+invariant3NFTSpent
+  policyId
+  redeemer
+  txInfo
+  voter
+  ref
+  address
+  datum
+  referenceScript
+  baseValue
+  baseTokens
+  token
+  quantity =
+    counterexample ("Context: " <> show ctx) $
+      hotCommitteeScript policyId redeemer ctx === True
+    where
+      tokens = AMap.insert token (max 1 quantity) baseTokens
+      value = Value $ AMap.insert policyId tokens $ getValue baseValue
+      txOut = TxOut address value datum referenceScript
+      input = TxInInfo ref txOut
+      txInfo' = txInfo{txInfoInputs = input : txInfoInputs txInfo}
+      purpose = Voting voter
+      ctx = ScriptContext txInfo' purpose
