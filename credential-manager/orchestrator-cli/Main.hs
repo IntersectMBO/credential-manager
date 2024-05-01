5b6e1200eca2aaead3b78373b452a0c444add8f4
Jamie Bertram <jamie.bertram@tweag.io>
2024-05-01 13:13:32 -0400

    Add unaudited warning to orchestrator-cli

--- credential-manager/orchestrator-cli/Main.hs
+++ credential-manager/orchestrator-cli/Main.hs
@@ -3,9 +3,14 @@ module Main where
 import Commands (runCommand)
 import Options (Options (..), options)
 import Options.Applicative (execParser)
+import System.IO (hPutStrLn, stderr)
 
 main :: IO ()
 main = execParser options >>= run
 
 run :: Options -> IO ()
-run Options{..} = runCommand command
+run Options{..} = do
+  hPutStrLn
+    stderr
+    "WARNING: This version of the credential management system has not been audited, and is not recommended for use on mainnet. "
+  runCommand command
