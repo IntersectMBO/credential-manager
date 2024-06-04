f48f3dc22a59218b255fb9ec54ed6d952bd49e1b
Jamie Bertram <jamie.bertram@tweag.io>
2024-06-04 15:27:33 -0400

    Add basic tool layout

--- credential-manager/signing-tool/Main.hs
+++ credential-manager/signing-tool/Main.hs
@@ -25,17 +25,53 @@ runApp = do
   appNetwork <- compile do
     activateE <- signalE0 app #activate
     let ?app = app
-     in void $ execute $ appMain <$ activateE
+     in void $ execute $ buildMainWindow <$ activateE
   actuate appNetwork
   exitWithInt =<< app.run Nothing
 
-appMain :: (?app :: G.Application) => MomentIO ()
-appMain = do
+buildMainWindow :: (?app :: G.Application) => MomentIO ()
+buildMainWindow = do
   window <- new G.ApplicationWindow [#application := ?app]
   window.setTitle $ Just "Transaction Signing Tool"
-  window.setDefaultSize 200 200
+
+  box <- new G.Box [#orientation := G.OrientationVertical]
+  box.setHalign G.AlignFill
+  box.setValign G.AlignFill
+  box.setMarginTop 8
+  box.setMarginStart 8
+  box.setMarginEnd 8
+  box.setMarginBottom 8
+  box.setSpacing 8
+  window.setChild $ Just box
+
+  txViewLabel <- new G.Label [#label := "Transaction summary"]
+  txViewLabel.setHalign G.AlignStart
+  box.append txViewLabel
+
+  txView <- new G.ScrolledWindow []
+  txView.setVexpand True
+  txView.setHasFrame True
+  box.append txView
+
+  mainButtons <- buildMainButtons
+  box.append mainButtons
 
   window.present
+
+buildMainButtons :: MomentIO G.Box
+buildMainButtons = do
+  box <- new G.Box [#orientation := G.OrientationHorizontal]
+  box.setHalign G.AlignStart
+  box.setValign G.AlignCenter
+  box.setSpacing 8
+
+  importTxButton <- new G.Button [#label := "Import transaction"]
+  box.append importTxButton
+
+  createKeyPairButton <- new G.Button [#label := "Create new key pair"]
+  box.append createKeyPairButton
+
+  pure box
 
 exitWithInt :: Int32 -> IO ()
 exitWithInt 0 = exitSuccess
