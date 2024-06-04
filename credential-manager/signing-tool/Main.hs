7506cb94a0dfb16aaed55c8652b244888cf8ef7d
Jamie Bertram <jamie.bertram@tweag.io>
2024-06-04 14:45:44 -0400

    Port and update reactive-banana-gi-gtk

--- credential-manager/signing-tool/Main.hs
+++ credential-manager/signing-tool/Main.hs
@@ -1,40 +1,39 @@
+{-# LANGUAGE ImplicitParams #-}
 {-# LANGUAGE OverloadedLabels #-}
 {-# LANGUAGE OverloadedRecordDot #-}
 
 module Main where
 
+import Control.Exception (catch)
+import Data.Functor (void)
 import Data.GI.Base
 import Data.Int (Int32)
+import qualified Data.Text as T
 import GHC.IO.Exception (ExitCode (ExitFailure))
 import qualified GI.Gtk as G
+import Reactive.Banana
+import Reactive.Banana.Frameworks
+import Reactive.Banana.GI.Gtk (signalE0)
 import System.Exit (exitSuccess, exitWith)
 
 main :: IO ()
-main = do
-  G.init
+main = runApp `catch` \(e :: GError) -> gerrorMessage e >>= putStrLn . T.unpack
 
+runApp :: IO ()
+runApp = do
   app <- new G.Application [#applicationId := "iog.signing-tool"]
-
-  _ <- on app #activate $ activate app
-
+  appNetwork <- compile do
+    activateE <- signalE0 app #activate
+    let ?app = app
+     in void $ execute $ appMain <$ activateE
+  actuate appNetwork
   exitWithInt =<< app.run Nothing
 
-activate :: G.Application -> IO ()
-activate app = do
-  window <- new G.ApplicationWindow [#application := app]
-  window.setTitle $ Just "CC Transaction Signing Tool"
+appMain :: (?app :: G.Application) => MomentIO ()
+appMain = do
+  window <- new G.ApplicationWindow [#application := ?app]
+  window.setTitle $ Just "Transaction Signing Tool"
   window.setDefaultSize 200 200
-
-  box <- new G.Box [#orientation := G.OrientationVertical]
-  box.setHalign G.AlignCenter
-  box.setValign G.AlignCenter
-  window.setChild $ Just box
-
-  button <- new G.Button [#label := "Hello World"]
-
-  _ <- on button #clicked (putStrLn "hello")
-
-  box.append button
 
   window.present
 
