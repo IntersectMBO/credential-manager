b334d9c29a33248a03ce37d1071313848a2539f1
Jamie Bertram <jamie.bertram@tweag.io>
2024-07-05 16:41:48 -0400

    Add statics to build

--- nix/outputs.nix
+++ nix/outputs.nix
@@ -4,11 +4,28 @@ let
 
   project = repoRoot.nix.project;
 
+  staticPkgs = project.cabalProject.projectCross.musl64.hsPkgs;
+
+  static = staticPkgs.credential-manager.components.exes;
+
+  allStatic = pkgs.runCommand "all-statics" { } ''
+    mkdir -p $out
+    ${lib.concatMapStringsSep "\n" (drv: "cp ${drv}/bin/* $out") (lib.attrValues static)}
+  '';
+
+
 in
 
 [
   (
     # Docs for project.flake: https://github.com/input-output-hk/iogx/blob/main/doc/api.md#mkhaskellprojectoutflake
     project.flake
+  )
+  (lib.optionalAttrs pkgs.stdenv.isLinux
+    {
+      packages.allStatic = allStatic;
+      hydraJobs.allStatic = allStatic;
+      hydraJobs.static = static;
+    }
   )
 ]
