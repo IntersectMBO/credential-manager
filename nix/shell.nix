6cb631205303ff28e8035a9da4c9d888b3e6eae3
Jamie Bertram <jamie.bertram@tweag.io>
2024-07-12 10:55:34 -0400

    Comment out hls patch

--- nix/shell.nix
+++ nix/shell.nix
@@ -29,22 +29,22 @@ cabalProject:
   };
 
   tools = {
-    haskell-language-server =
-      let
-        hlsProject = pkgs.haskell-nix.cabalProject' {
-          name = "haskell-language-server";
-          src = inputs.iogx.inputs.haskell-nix.inputs."hls-2.6";
-          configureArgs = "--disable-benchmarks --disable-tests";
-          compiler-nix-name = lib.mkDefault "ghc96";
-          modules = [{
-            packages.ghcide.patches = [
-              # https://github.com/haskell/haskell-language-server/issues/4046#issuecomment-1926242056
-              ./ghcide-workaround.diff
-            ];
-          }];
-        };
-      in
-      hlsProject.hsPkgs.haskell-language-server.components.exes.haskell-language-server;
+    # haskell-language-server =
+    #   let
+    #     hlsProject = pkgs.haskell-nix.cabalProject' {
+    #       name = "haskell-language-server";
+    #       src = inputs.iogx.inputs.haskell-nix.inputs."hls-2.6";
+    #       configureArgs = "--disable-benchmarks --disable-tests";
+    #       compiler-nix-name = lib.mkDefault "ghc96";
+    #       modules = [{
+    #         packages.ghcide.patches = [
+    #           # https://github.com/haskell/haskell-language-server/issues/4046#issuecomment-1926242056
+    #           ./ghcide-workaround.diff
+    #         ];
+    #       }];
+    #     };
+    #   in
+    #   hlsProject.hsPkgs.haskell-language-server.components.exes.haskell-language-server;
   };
 
   # prompt = null;
