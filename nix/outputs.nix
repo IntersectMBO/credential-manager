1262af5158328e716091860142f7657a5c2ee9cd
Jamie Bertram <jamie.bertram@tweag.io>
2024-07-15 13:51:11 -0400

    only build orchestrator-cli statically

--- nix/outputs.nix
+++ nix/outputs.nix
@@ -4,14 +4,7 @@ let
 
   project = repoRoot.nix.project;
 
-  staticPkgs =
-    if pkgs.stdenv.isLinux
-    then project.cabalProject.projectCross.musl64.hsPkgs
-    else if system == "x86_64-darwin"
-    then project.cabalProject.projectCross.x86_64-darwin.hsPkgs
-    else if system == "aarch64-darwin"
-    then project.cabalProject.projectCross.aarch64-darwin.hsPkgs
-    else { };
+  staticPkgs = project.cabalProject.projectCross.musl64.hsPkgs;
 
   static = staticPkgs.credential-manager.components.exes;
 
@@ -22,7 +15,7 @@ in
     # Docs for project.flake: https://github.com/input-output-hk/iogx/blob/main/doc/api.md#mkhaskellprojectoutflake
     project.flake
   )
-  {
+  (lib.optionalAttrs pkgs.stdenv.isLinux {
     hydraJobs.static.orchestrator-cli = static.orchestrator-cli;
-  }
+  })
 ]
