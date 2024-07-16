040e40651587c33a61d62ac98224ee39e6790dc2
Jamie Bertram <jamie.bertram@tweag.io>
2024-07-16 16:29:05 -0400

    Add wrapGAppsHook to signing-tool

--- nix/outputs.nix
+++ nix/outputs.nix
@@ -13,7 +13,13 @@ in
 [
   (
     # Docs for project.flake: https://github.com/input-output-hk/iogx/blob/main/doc/api.md#mkhaskellprojectoutflake
-    project.flake
+    lib.recursiveUpdate project.flake rec {
+      packages.signing-tool =
+        project.cabalProject.hsPkgs.credential-manager.components.exes.signing-tool.overrideAttrs
+          (final: prev: {
+            nativeBuildInputs = prev.nativeBuildInputs ++ [ pkgs.wrapGAppsHook4 ];
+          });
+    }
   )
   (lib.optionalAttrs pkgs.stdenv.isLinux {
     hydraJobs.static.orchestrator-cli = static.orchestrator-cli;
