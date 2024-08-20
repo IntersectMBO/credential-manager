92326ba5c6cdcc15a3b8770feac2bb5ab319b7b9
Jamie Bertram <jamie.bertram@tweag.io>
2024-08-20 14:11:13 -0400

    Add musl64 jobs

--- nix/outputs.nix
+++ nix/outputs.nix
@@ -4,17 +4,11 @@ let
 
   project = repoRoot.nix.project;
 
-  projectCross = project.cabalProject.projectCross;
-
-  staticPkgs = project.cabalProject.projectCross.musl64.hsPkgs;
-
-  static = staticPkgs.credential-manager.components.exes;
-
 in
 
 [
   project.flake
   (lib.optionalAttrs pkgs.stdenv.isLinux {
-    hydraJobs.static.orchestrator-cli = static.orchestrator-cli;
+    hydraJobs.musl64 = project.cross.musl64.hydraJobs;
   })
 ]
