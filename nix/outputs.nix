306432246517ed75544cafedbea87a56ccaceebe
Jamie Bertram <jamie.bertram@tweag.io>
2024-09-04 10:27:21 -0400

    Fix cc-sign-native in nix expression

--- nix/outputs.nix
+++ nix/outputs.nix
@@ -3,6 +3,22 @@
 let
 
   project = repoRoot.nix.project;
+  cc-sign-native = pkgs.runCommand "cc-sign"
+    {
+      nativeBuildInputs = [
+        pkgs.macdylibbundler
+        pkgs.darwin.autoSignDarwinBinariesHook
+      ];
+    } ''
+    mkdir -p $out/bin
+    cp ${project.packages.cc-sign}/bin/cc-sign $out/bin/cc-sign
+    chmod 755 $out/bin/cc-sign
+    dylibbundler -b --no-codesign \
+      -x $out/bin/cc-sign \
+      -d $out/bin \
+      -p '@executable_path'
+    signDarwinBinariesInAllOutputs
+  '';
 
 in
 
@@ -11,23 +27,8 @@ in
   (lib.optionalAttrs pkgs.stdenv.isLinux {
     hydraJobs.musl64 = project.cross.musl64.hydraJobs;
   })
-  (lib.optionalAttrs pkgs.stdenv.isDarwin (rec {
-    packages.cc-sign-native = pkgs.runCommand "cc-sign"
-      {
-        nativeBuildInputs = [
-          pkgs.macdylibbundler
-          pkgs.darwin.autoSignDarwinBinariesHook
-        ];
-      } ''
-      mkdir -p $out/bin
-      cp ${project.packages.cc-sign}/bin/cc-sign $out/bin/cc-sign
-      chmod 755 $out/bin/cc-sign
-      dylibbundler -b --no-codesign \
-        -x $out/bin/cc-sign \
-        -d $out/bin \
-        -p '@executable_path'
-      signDarwinBinariesInAllOutputs
-    '';
-    hydraJobs.cc-sign-native = cc-sign-native;
-  }))
+  (lib.optionalAttrs pkgs.stdenv.isDarwin {
+    packages = { inherit cc-sign-native; };
+    hydraJobs = { inherit cc-sign-native; };
+  })
 ]
