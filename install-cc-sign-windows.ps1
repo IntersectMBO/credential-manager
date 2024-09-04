751f73234da0619338d90cb88a6be5911951392c
Jamie Bertram <jamie.bertram@tweag.io>
2024-09-04 16:38:47 -0400

    Force extraction

--- install-cc-sign-windows.ps1
+++ install-cc-sign-windows.ps1
@@ -1,5 +1,5 @@
 Invoke-RestMethod -Uri "https://github.com/IntersectMBO/credential-manager/releases/download/0.1.1.0/cc-sign-windows.zip" -OutFile cc-sign-windows.zip
-Expand-Archive cc-sign-windows.zip -DestinationPath $env:USERPROFILE\cc-sign
+Expand-Archive cc-sign-windows.zip -DestinationPath $env:USERPROFILE\cc-sign -Force
 [System.Environment]::SetEnvironmentVariable("Path", "$env:Path;$env:USERPROFILE\cc-sign\result\bin", [System.EnvironmentVariableTarget]::User)
 $env:Path += ";$env:USERPROFILE\cc-sign\result\bin"
 Remove-Item cc-sign-windows.zip
