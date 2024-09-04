d01cfdbebcceab1db8d5f5b6446b8336ee7fe4f3
Jamie Bertram <jamie.bertram@tweag.io>
2024-09-04 10:17:40 -0400

    Add native mac build of cc-sign

--- nix/outputs.nix
+++ nix/outputs.nix
@@ -11,4 +11,23 @@ in
   (lib.optionalAttrs pkgs.stdenv.isLinux {
     hydraJobs.musl64 = project.cross.musl64.hydraJobs;
   })
+  (lib.optionalAttrs pkgs.stdenv.isDarwin (rec {
+    packages.cc-sign-native = pkgs.runCommand "cc-sign"
+      {
+        nativeBuildInputs = [
+          pkgs.macdylibbundler
+          pkgs.darwin.autoSignDarwinBinariesHook
+        ];
+      } ''
+      mkdir -p $out/bin
+      cp ${project.packages.cc-sign}/bin/cc-sign $out/bin/cc-sign
+      chmod 755 $out/bin/cc-sign
+      dylibbundler -b --no-codesign \
+        -x $out/bin/cc-sign \
+        -d $out/bin \
+        -p '@executable_path'
+      signDarwinBinariesInAllOutputs
+    '';
+    hydraJobs.cc-sign-native = cc-sign-native;
+  }))
 ]
