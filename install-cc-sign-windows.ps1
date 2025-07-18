25e46998a2d0681bdeb5de6c70e32802c684ca50
perturbing <thomas.vellekoop@iohk.io>
2025-07-18 18:23:11 +0200

    Update isntall scripts

--- install-cc-sign-windows.ps1
+++ install-cc-sign-windows.ps1
@@ -1,4 +1,4 @@
-Invoke-RestMethod -Uri "https://github.com/IntersectMBO/credential-manager/releases/download/0.1.3.0/cc-sign-windows.zip" -OutFile cc-sign-windows.zip
+Invoke-RestMethod -Uri "https://github.com/IntersectMBO/credential-manager/releases/download/0.1.4.0/cc-sign-windows.zip" -OutFile cc-sign-windows.zip
 Expand-Archive cc-sign-windows.zip -DestinationPath $env:USERPROFILE\cc-sign -Force
 [System.Environment]::SetEnvironmentVariable("Path", "$env:Path;$env:USERPROFILE\cc-sign\result\bin", [System.EnvironmentVariableTarget]::User)
 $env:Path += ";$env:USERPROFILE\cc-sign\result\bin"
