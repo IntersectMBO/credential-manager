167e65b03f5955410337cd550c46c864aa935791
Thomas Vellekoop <107037423+perturbing@users.noreply.github.com>
2026-06-01 16:12:16 +0200

    Refactor install script for better temp handling

--- install-cc-sign-mac-os.sh
+++ install-cc-sign-mac-os.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
-#
+set -euo pipefail
+
 ARCH="$(arch)"
-if [[ "$ARCH" == "x86_64" ]] || [[ "$ARCH" == "i386" ]];
-then
+
+if [[ "$ARCH" == "x86_64" ]] || [[ "$ARCH" == "i386" ]]; then
   ZIP_URL="https://github.com/IntersectMBO/credential-manager/releases/download/0.1.5.0/cc-sign-mac-os-intel.zip"
 else
   ZIP_URL="https://github.com/IntersectMBO/credential-manager/releases/download/0.1.5.0/cc-sign-mac-os-arm.zip"
 fi
 
-curl -L --output /tmp/cc-sign.zip $ZIP_URL
-unzip /tmp/cc-sign.zip -d /tmp
+tmpdir="$(mktemp -d)"
+trap 'rm -rf "$tmpdir"' EXIT
+
+curl -fL --proto '=https' --tlsv1.2 --output "$tmpdir/cc-sign.zip" "$ZIP_URL"
+
+unzip -q "$tmpdir/cc-sign.zip" -d "$tmpdir"
+
 mkdir -p /usr/local/lib
-cp -n /tmp/result/bin/*.dylib /usr/local/lib
-chmod +w /tmp/result/bin/cc-sign
+cp -n "$tmpdir/result/bin/"*.dylib /usr/local/lib/
+
+chmod +w "$tmpdir/result/bin/cc-sign"
+
 mkdir -p /usr/local/bin
-cp /tmp/result/bin/cc-sign /usr/local/bin
-rm -rf /tmp/result
+cp "$tmpdir/result/bin/cc-sign" /usr/local/bin/cc-sign
